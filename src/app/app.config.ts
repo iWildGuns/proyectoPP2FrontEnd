35c6e9830b5f39035cdba9b476de148b3e798e40
Nacho P <jip224@gmail.com>
2026-06-26 12:13:34 -0300

    agregado formato de moneda peso argentino de manera global

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,17 @@
+import { registerLocaleData } from '@angular/common';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import localeEsAr from '@angular/common/locales/es-AR';
 import {
   ApplicationConfig,
+  LOCALE_ID,
   provideBrowserGlobalErrorListeners
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
 import { notificationInterceptor } from './interceptors/notification.interceptor';
+
+registerLocaleData(localeEsAr);
 
 export const appConfig: ApplicationConfig = {
 
@@ -19,5 +24,6 @@ export const appConfig: ApplicationConfig = {
       positionClass: 'toast-top-center',
       preventDuplicates: true,
     }),
+    { provide: LOCALE_ID, useValue: 'es-AR' }
   ]
 };
